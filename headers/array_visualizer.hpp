53c4e63399d80cfc890ff0f6a6a87a9e3efb9025
westelh <reo.retriever@gmail.com>
2017-12-08 20:20:53 +0900

    abolishing data_elem data access

--- headers/array_visualizer.hpp
+++ headers/array_visualizer.hpp
@@ -4,6 +4,7 @@
 #include <array>
 #include <algorithm>
 #include <random>
+#include <memory>
 #include "line.hpp"
 
 namespace {
@@ -15,82 +16,48 @@ template <int N>
 class array_visualizer: public shape {
 	static constexpr float data_scaling = 7.5f;
 	static constexpr float margin_coef = 10.0f;
-
-	class data_elem {
-		int *const dataref;
-		line *const lineref;
-	public:
-		data_elem(int *const d, line *const l);
-	 	data_elem& operator=(int i);
-	 	data_elem& operator=(const data_elem& d);
-	 	operator int() const noexcept;
-	};
-
-	std::array<int, N> data;
+	
+	const std::shared_ptr<std::array<int, N>> data_m;
 	std::array<line, N> lines;
 
 public:
-	array_visualizer(coord pos = coord{100, 100});
+	array_visualizer(const std::shared_ptr<std::array<int, N>>& data, coord pos = coord{100, 100});
 	virtual void draw() noexcept override;
 
-	data_elem at(size_t i);
-	data_elem operator[](size_t i);
+protected:
+	int fixed_line_length(size_t data_index) const noexcept;
 };
 
 
 //////////////////////////////////////////////////////////
 // -- implementation --
 template <int N>
-array_visualizer<N>::array_visualizer(coord pos): shape(pos, 0) {
-	for (size_t i=0; data.size() > i; ++i) {
-		data.at(i) = static_cast<int>(i);
+array_visualizer<N>::array_visualizer(const std::shared_ptr<std::array<int, N>>& data, coord pos): 
+	shape(pos, 0), data_m{data} {
+
+	for (size_t i=0; data_m->size() > i; ++i) {
+		data_m->at(i) = static_cast<int>(i);
 	}
-	std::shuffle(data.begin(), data.end(), std::mt19937{std::random_device{}()});
+	std::shuffle(data->begin(), data->end(), std::mt19937{std::random_device{}()});
     for (size_t i=0; lines.size() > i; ++i) {
-        lines.at(i).set_coord(coord(pos.first+static_cast<int>(static_cast<int>(i)*margin_coef), pos.second));
+        lines.at(i).set_coord(coord(pos.first+static_cast<int>(i*margin_coef), pos.second));
         lines.at(i).set_angle(-pi<float>/2);
-        lines.at(i).set_length(static_cast<int>(data.at(i)*data_scaling));
+        lines.at(i).set_length(fixed_line_length(i));
     }
 }
 
 template <int N>
 void array_visualizer<N>::draw() noexcept {
-	for (auto&& i : lines) {
-		i.set_rgb(r, g, b);
-		i.draw();
+	for(unsigned i = 0; i < N; ++i) {
+		lines.at(i).set_length(fixed_line_length(i));
+		lines.at(i).set_rgb(r, g, b);
+		lines.at(i).draw();
 	}
 }
 
 template <int N>
-typename array_visualizer<N>::data_elem array_visualizer<N>::at(size_t i) {
-	return data_elem{&data.at(i), &lines.at(i)};
-}
-
-template <int N>
-typename array_visualizer<N>::data_elem array_visualizer<N>::operator[](size_t i) {
-	return at(i);
-}
-
-template <int N>
-array_visualizer<N>::data_elem::data_elem(int *const d, line *const l): dataref{d}, lineref{l} { }
-
-template <int N>
-typename array_visualizer<N>::data_elem& array_visualizer<N>::data_elem::operator=(int i) {
-	*dataref = i;
-	lineref->set_length(static_cast<int>(i*data_scaling));
-	return *this;
-}
-
-template <int N>
-typename array_visualizer<N>::data_elem& array_visualizer<N>::data_elem::operator=(const data_elem& d) {
-	*dataref = static_cast<int>(d);
-	lineref->set_length(static_cast<int>(static_cast<int>(d)*data_scaling));
-	return *this;
-}
-
-template <int N>
-array_visualizer<N>::data_elem::operator int() const noexcept {
-	return *dataref;
+int array_visualizer<N>::fixed_line_length(size_t data_index) const noexcept {
+	return static_cast<int>(data_m->at(data_index)*data_scaling);
 }
 
 #endif // ARRAY_VISUALIZER_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -13,16 +13,17 @@ int main(int argc, char** argv) {
     allegro::init();
     allegro::show_window(800, 600);
 
-    array_visualizer<data_size> array(pos(50, 800));
+    auto data = std::make_shared<std::array<int, data_size>>();
+    array_visualizer<data_size> visual(data, pos(50, 800));
 
     bool stop = false;
     std::thread th{[&](){
         for (size_t i=0; i<data_size; ++i) {
             for (size_t j=0; j<(data_size-i)-1; ++j) {
-                if (array[j]>array[j+1]) {
-                    int temp = array.at(j);
-                    array.at(j) = array.at(j+1);
-                    array.at(j+1) = temp;
+                if (data->at(j)>data->at(j+1)) {
+                    int temp = data->at(j);
+                    data->at(j) = data->at(j+1);
+                    data->at(j+1) = temp;
 
                     std::this_thread::sleep_for(std::chrono::milliseconds(3));
                 }
@@ -30,13 +31,13 @@ int main(int argc, char** argv) {
             }
             if (stop) break;
         }
-        array.set_rgb(0, 255, 0);
+        visual.set_rgb(0, 255, 0);
     }};
 
     while(allegro::check_exit()) {
         if(allegro::need_refresh()) {
             allegro::fill_with(0, 0, 0);
-            array.draw();
+            visual.draw();
         }
         allegro::internal_proc();
     }
